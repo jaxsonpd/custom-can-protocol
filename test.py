e491095305c2e5730b1b340af2294f3fecacb008
Jack Duignan <jackpduignan@gmail.com>
2025-03-09 13:21:55 +1300

    Update Test

--- test.py
+++ test.py
@@ -6,9 +6,11 @@ port = serial.Serial(port="/dev/ttyUSB0", baudrate=115200)
 
 def read_thread():
     while True:
-        data = port.read_all()
+        data = port.read_until(bytes([127]))
         if (data != b''):
-            print(data)
+            for byte in data:
+                print(int(byte), end=' ')
+            print()
 
 
 read_thread = threading.Thread(target=read_thread)
@@ -19,9 +21,9 @@ packet = bytes([126, 4, 0, 255, 255, 127])
 packet2 = b'~\x01\x08\x00w\x01\x13\x00v\x00\x00i\x9b\x7f'
 
 while(True):
-    for x in packet2:
-        port.write(bytes([x]))
-        sleep(0.1)
+    # for x in packet2:
+    #     port.write(bytes([x]))
+    #     sleep(0.1)
 
     sleep(2)
     # port.write(packet)
